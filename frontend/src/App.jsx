cc50997dec55ffee2f16ca6853bdfee4a52cef5d
dewa <arisurya228@gmail.com>
2026-02-27 14:42:57 +0700

    Login Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/Login";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/about" element={<h1>About</h1>} />
+      <Route path="/auth" element={<Login />} />
       <Route path="/contact" element={<h1>Contact</h1>} />
     </Routes>
   );
